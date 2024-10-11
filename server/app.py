7df22138c5015f19a99cc5324485af23aac6590f
crea-tivecoder <nyawirawaweru.com>
2024-10-11 18:18:13 +0300

    Create_Retrieve Restful lab

--- server/app.py
+++ server/app.py
@@ -17,11 +17,43 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants=[plant.to_dict() for plant in Plant.query.all()]
+        response=make_response(
+            plants,
+            200
+        )
+        return response
+    
+    def post(self):
+        data=request.get_json()
 
-class PlantByID(Resource):
-    pass
+        new_plant=Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price']
+        )
+        db.session.add(new_plant)
+        db.session.commit()
         
+        response_body=new_plant.to_dict()
+        response=make_response(
+            response_body,
+            201
+        )
+        return response
+
+api.add_resource(Plants, '/plants')
+    
+class PlantByID(Resource):
+    def get(self, id):
+        plant=Plant.query.filter_by(id=id).first().to_dict()
+        response=make_response(
+            plant,
+            200
+        )
+        return response
+api.add_resource(PlantByID, '/plants/<int:id>')       
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
